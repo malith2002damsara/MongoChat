0d7bf1272d952051805381ccaa83a2fd23b88a5e
malith damsara <malithdamsara87@gmail.com>
2025-08-10 16:36:56 +0530

    aaa

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-desktop-backend.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mongochatbackend.vercel.app",
   withCredentials: false, // Disable credentials for Vercel compatibility
   headers: {
     'Content-Type': 'application/json',
